9835b09c3f0560d8025af3540683a716d7eb9cdd
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2017-10-29 15:10:36 +0100

    [Maps] [GestControllers]
    Modificado comportamiento del mapa
    Creados controladores para páginas de invitados.

--- Software/Codigo/src/main/java/com/motostiburon/controller/IndexController.java
+++ Software/Codigo/src/main/java/com/motostiburon/controller/IndexController.java
@@ -9,18 +9,55 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
 	// REFERENCIAS A CONTENEDORES
-	private static final String CONTENEDOR = "views/motostiburon/home/inicial";
+	private static final String CONTENEDOR_INICIAL = "views/motostiburon/home/inicial";
+	private static final String CONTENEDOR_EMPRESA = "views/motostiburon/home/empresa";
+	private static final String CONTENEDOR_SERVICIOS = "views/motostiburon/home/servicios";
+	private static final String CONTENEDOR_CLIENTES = "views/motostiburon/home/clientes";
+	
 	private static final String FRAGMENTO = "fragment";
 
 	@RequestMapping("/home")
-	public ModelAndView show() {
+	public ModelAndView showHome() {
 		ModelAndView view = new ModelAndView("index");
 		
-		view.addObject("contenedor", CONTENEDOR);
+		view.addObject("contenedor", CONTENEDOR_INICIAL);
 		view.addObject("fragmento", FRAGMENTO);
 		
 		return view;
 	}
+	
+	@RequestMapping("/empresa")
+	public ModelAndView showEmpresa() {
+		ModelAndView view = new ModelAndView("index");
+		
+		view.addObject("contenedor", CONTENEDOR_EMPRESA);
+		view.addObject("fragmento", FRAGMENTO);
+		
+		return view;
+	}
+	
+	@RequestMapping("/servicios")
+	public ModelAndView showServicios() {
+		ModelAndView view = new ModelAndView("index");
+		
+		view.addObject("contenedor", CONTENEDOR_SERVICIOS);
+		view.addObject("fragmento", FRAGMENTO);
+		
+		return view;
+	}
+	
+	
+	@RequestMapping("/clientes")
+	public ModelAndView showClientes() {
+		ModelAndView view = new ModelAndView("index");
+		
+		view.addObject("contenedor", CONTENEDOR_CLIENTES);
+		view.addObject("fragmento", FRAGMENTO);
+		
+		return view;
+	}
+	
+	
 	
 	
 }
